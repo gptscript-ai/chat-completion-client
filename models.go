bdef9f2226a91728b6a763e37855b337113c41a5
Darren Shepherd <darren@acorn.io>
2024-05-14 22:05:33 -0700

    chore: add gpt4o model strings

--- models.go
+++ models.go
@@ -17,6 +17,8 @@ const (
 	GPT40314              = "gpt-4-0314"
 	GPT4Turbo             = "gpt-4-turbo"
 	GPT4Turbo20240409     = "gpt-4-turbo-2024-04-09"
+	GPT4o                 = "gpt-4o"
+	GPT4o20240513         = "gpt-4o-2024-05-13"
 	GPT4Turbo0125         = "gpt-4-0125-preview"
 	GPT4Turbo1106         = "gpt-4-1106-preview"
 	GPT4TurboPreview      = "gpt-4-turbo-preview"
